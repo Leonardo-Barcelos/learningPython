d6af64704f5fb1d36cb236eb6adadac0666753a4
Leonardo <d3pro3@outlook.com>
2022-07-25 14:42:57 -0300

    for loops finalizada

--- conditionalsAndRepetitionStructures/for_loops.py
+++ conditionalsAndRepetitionStructures/for_loops.py
@@ -28,4 +28,59 @@ for x in fruits:
   if x == "banana":
     break
   print(x)
+print('--------------------------------------------------')
+
+
+# The continue Statement
+
+## With the continue statement we can stop the current iteration of the loop, and continue with the next:
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+print('--------------------------------------------------')  
+
+# The range() Function
+
+## To loop through a set of code a specified number of times, we can use the range() function,
+
+for x in range(6):
+  print(x)
+### Note that range(6) is not the values of 0 to 6, but the values 0 to 5       
+
+print('--------------------------------------------------')
+
+for x in range(2, 6):
+  print(x)  
+  
+ # Else in For Loop
+  
+  ## The else keyword in a for loop specifies a block of code to be executed when the loop is finished:
+  
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+  
+ ## The else block will NOT be executed if the loop is stopped by a break statement
+ 
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!") 
+  
+# Nested Loops
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)  
     
+# The pass Statement
+for x in [0, 1, 2]:
+  pass    
