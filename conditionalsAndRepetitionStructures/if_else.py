dd58b36097534240273f748471310f20ee9c6ec6
Leonardo <d3pro3@outlook.com>
2022-07-25 13:52:36 -0300

    explicação do elif

--- conditionalsAndRepetitionStructures/if_else.py
+++ conditionalsAndRepetitionStructures/if_else.py
@@ -16,7 +16,7 @@ print("-------------------------------------")
 
 if x > y:
     print(" %s is greater then %s" %(x,y)) 
-elif x < y:
+elif x < y: # The elif keyword is pythons way of saying "if the previous conditions were not true, then try this condition".
     print(print(" %s is greater then %s" %(y,x)))
 else:
     print("They are the same")
