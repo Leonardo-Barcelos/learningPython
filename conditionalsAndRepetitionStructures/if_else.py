0ddfae1a65c6545ac30aeae4f9d5735ae1f597b4
Leonardo <d3pro3@outlook.com>
2022-07-25 16:02:04 -0300

    if correcao

--- conditionalsAndRepetitionStructures/if_else.py
+++ conditionalsAndRepetitionStructures/if_else.py
@@ -1,6 +1,6 @@
 """
 Python relies on indentation (whitespace at the beginning of a line)
-to define scope in the code. 
+to define scope in the code.
 Other programming languages often use curly-brackets for this purpose.
 
 # to concat use %s <-- IMPORTANT
@@ -11,20 +11,20 @@ y = 7999
 print("-------------------------------------")
 
 if x > y:
-    print("Your power is over  %s !!!!!" %(x))     
+    print("Your power is over  %s !!!!!" % (x))
 print("-------------------------------------")
 
 if x > y:
-    print(" %s is greater then %s" %(x,y)) 
-elif x < y: # The elif keyword is pythons way of saying "if the previous conditions were not true, then try this condition".
-    print(print(" %s is greater then %s" %(y,x)))
+    print(" %s is greater then %s" % (x, y))
+elif x < y:  # The elif keyword is pythons way of saying "if the previous conditions were not true, then try this condition".
+    print(print(" %s is greater then %s" % (y, x)))
 else:
     print("They are the same")
-    
+
 print("-------------------------------------")
 
 # Short Hand If
-if x > y: print("%s is greater than %s" %(x,y))
+if x > y:print("%s is greater than %s" % (x, y))
 
 # Short Hand If ... Else
 print("X") if x > y else print("Y")
